b11dda7eea702056992be38f3fbf0f59014817ec
Dezzlar <dimorhoa@gmail.com>
2023-09-24 22:48:56 +0100

    Updates

---
+++ precache-manifest.1a58c6ad72dae90623a9ca79fa113eb9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b6f1892153d411ec7f0",
+    "url": "/Robo_Friends/static/js/0.57632ce4.chunk.js"
+  },
+  {
+    "revision": "05d85cb591e98df63d97",
+    "url": "/Robo_Friends/static/css/main.63d26824.chunk.css"
+  },
+  {
+    "revision": "05d85cb591e98df63d97",
+    "url": "/Robo_Friends/static/js/main.6e67b8bc.chunk.js"
+  },
+  {
+    "revision": "770a305b4743b578770f",
+    "url": "/Robo_Friends/static/js/runtime~main.ff9445b3.js"
+  },
+  {
+    "revision": "27a46c94ddd051f0b205",
+    "url": "/Robo_Friends/static/css/3.a8fa396c.chunk.css"
+  },
+  {
+    "revision": "27a46c94ddd051f0b205",
+    "url": "/Robo_Friends/static/js/3.b23e44a2.chunk.js"
+  },
+  {
+    "revision": "56271cd77110d1f8b306",
+    "url": "/Robo_Friends/static/js/4.7a445e94.chunk.js"
+  },
+  {
+    "revision": "1bd381e72cb8332ea226",
+    "url": "/Robo_Friends/static/js/5.942866bc.chunk.js"
+  },
+  {
+    "revision": "c155524824a0ac279991",
+    "url": "/Robo_Friends/static/js/6.8989e8a8.chunk.js"
+  },
+  {
+    "revision": "33cf3380e75136e57627",
+    "url": "/Robo_Friends/static/js/7.fda7342b.chunk.js"
+  },
+  {
+    "revision": "a498e4ad83c649f2f00a",
+    "url": "/Robo_Friends/static/js/8.85b4ac8e.chunk.js"
+  },
+  {
+    "revision": "b9015c02cd66c6e7df54",
+    "url": "/Robo_Friends/static/js/9.b42417e6.chunk.js"
+  },
+  {
+    "revision": "9450a0a023f771f6caed",
+    "url": "/Robo_Friends/static/js/10.f46e2006.chunk.js"
+  },
+  {
+    "revision": "9f965aa8dc973f6a332a",
+    "url": "/Robo_Friends/static/js/11.7eb22d30.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo_Friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d5e0a0f0c039d0666f72916a1569424a",
+    "url": "/Robo_Friends/index.html"
+  }
+];
