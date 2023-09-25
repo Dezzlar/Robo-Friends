e6852dee5a7f5e904d62d261526e47e1883f86ef
Dezzlar <dimorhoa@gmail.com>
2023-09-25 03:49:28 +0100

    Updates

---
+++ precache-manifest.d30643d797f8992be2996d9a9678ded8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b08b14c67d7193021f",
+    "url": "/Robo-Friends/static/css/main.b106e8aa.chunk.css"
+  },
+  {
+    "revision": "e5b08b14c67d7193021f",
+    "url": "/Robo-Friends/static/js/main.789e3010.chunk.js"
+  },
+  {
+    "revision": "7cc93a51a0c32a57fa20",
+    "url": "/Robo-Friends/static/js/runtime~main.0aea0349.js"
+  },
+  {
+    "revision": "7682b57cb7072fdcd5a7",
+    "url": "/Robo-Friends/static/css/2.769aa6b9.chunk.css"
+  },
+  {
+    "revision": "7682b57cb7072fdcd5a7",
+    "url": "/Robo-Friends/static/js/2.cb812774.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/Robo-Friends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "03dbfc850136ee97c000d80cdb8b8b0d",
+    "url": "/Robo-Friends/index.html"
+  }
+];
